e546dca8f45ccaa653acaed707f097c912db71fa
VishnupriyaGunasekaran2003 <gvishnupriya506@gmail.com>
2024-09-09 16:36:38 +0530

    End of Section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController] // localhost:5000/weatherforecast
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
